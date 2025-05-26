33f0a9c1b3494c0aa5cc120350f39625018c1f96
Davcid1572 <ifejiofordavid1572@gmail.com>
2025-05-26 12:02:27 +0100

    portfolio

--- script.js
+++ script.js
@@ -5,3 +5,50 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+const toggleButton = document.getElementById("dark-mode-toggle");
+
+toggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  //Save preference to localStorage
+  const isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDark ? "enabled" : "disabled");
+
+  //Change button icon
+  toggleButton.textContent = isDark ? "☀️" : "🌙";
+});
+
+// Load saved preference on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const savedMode = localStorage.getItem("darkMode");
+  if (savedMode === "enabled") {
+    document.body.classList.add("dark-mode");
+    toggleButton.textContent = "☀️";
+  }
+});
+
+const hamToggleButton = document.getElementById("ham-dark-mode-toggle");
+
+hamToggleButton.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  //Save preference to localStorage
+  const isDark = document.body.classList.contains("dark-mode");
+  localStorage.setItem("darkMode", isDark ? "enabled" : "disabled");
+
+  //Change button icon
+  toggleButton.textContent = isDark ? "☀️" : "🌙";
+});
+
+// Load saved preference on page load
+window.addEventListener("DOMContentLoaded", () => {
+  const savedMode = localStorage.getItem("darkMode");
+  if (savedMode === "enabled") {
+    document.body.classList.add("dark-mode");
+    toggleButton.textContent = "☀️";
+  }
+});
+
+// Used to update the year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
